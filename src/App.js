4d936dc76e4bf3c469674f7e0b2fd28ecdb847f2
jihoon <jihoon_yang@ksmartech.com>
2020-06-16 15:35:54 +0900

    finished chap06

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import axios from 'axios';
+import Movie from './Movie';
+import './App.css';
 
 class App extends React.Component {
   state = {
@@ -6,16 +9,48 @@ class App extends React.Component {
     movies: [],
   };
 
+  getMovie = async() =>{
+    const {
+      data:{
+        data:{movies},
+      },
+    } = await axios.get('https://yts.mx/api/v2/list_movies.json?sort_by=rating');
+
+    //this.setState({movies: movies});
+    this.setState({movies, isLoading:false});
+  }
+
   componentDidMount(){
     // 영화 데이터 로딩
-    setTimeout( () => {
-      this.setState({isLoading: false});
-    }, 6000);
+    this.getMovie();
   };
 
-  render(){
-    const {isLoading } = this.state;
-    return <div>{isLoading ? 'Loading...' : 'We are ready'} </div>;
+  render() {
+    const { isLoading, movies } = this.state;
+    return (
+      <section class="container">
+        {isLoading ? (
+          <div class="loader">
+            <span class="loader__text">
+              'Loading...'
+            </span>
+          </div>
+        ) : (
+            <div class="movies">
+              {movies.map(movie => (
+                <Movie
+                  key={movie.id}
+                  year={movie.year}
+                  title={movie.title}
+                  summary={movie.summary}
+                  poster={movie.medium_cover_image}
+                />
+              ))}
+            </div>
+
+          )}
+      </section>
+    );
   }
 }
 
